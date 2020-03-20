186c383b4da3277f31aa091e187642b57ece833c
Marces Engel <mengel@incoqnito.io>
2020-03-20 11:44:34 +0100

    Project setup

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+
+module.exports = {
+  context: path.resolve(__dirname, './src'),
+  entry: path.resolve(__dirname, './src/index.tsx'),
+  output: {
+    filename: 'app.[hash:12].js'
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.jsx', '.js' ],
+  },
+  module: {
+    rules: [ {
+      test: /\.tsx?/,
+      exclude: /node_modules/,
+      use: 'babel-loader'
+    } ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './index.html' }),
+    new CleanWebpackPlugin(),
+    new ForkTsCheckerWebpackPlugin({
+      tsconfig: path.resolve(__dirname, './tsconfig.json')
+    })
+  ]
+}
